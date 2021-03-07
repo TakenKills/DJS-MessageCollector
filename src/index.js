2e1a2ced45e2db08b797a2ea3cabd2db9f52979d
TakenKills <dragonagario2@gmail.com>
2021-03-07 13:07:16 +0200

    Removed the javascript files

--- src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const MessageCollector_1 = require("./Classes/MessageCollector");
-exports.default = MessageCollector_1.messageCollector;
